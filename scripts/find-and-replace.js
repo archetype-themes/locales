1486d71a9fd5a307b200fbb0f4c004d62cf859ba
Thomas Kelly <thomas@archetypethemes.co>
2023-12-27 15:06:59 -0500

    fix scripts back to working with nested objects

--- scripts/find-and-replace.js
+++ scripts/find-and-replace.js
@@ -12,49 +12,57 @@ const path = require('path');
 const glob = require('glob');
 
 function readJsonFile(filePath) {
-    const rawData = fs.readFileSync(filePath);
-    return JSON.parse(rawData);
+    try {
+        return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    } catch (error) {
+        console.error(`Error reading file ${filePath}:`, error);
+        process.exit(1);
+    }
 }
 
 function findAllKeyPaths(obj, targetValue, currentPath = '', paths = []) {
-  for (let key in obj) {
-      const value = obj[key];
-      const newPath = currentPath ? `${currentPath}.${key}` : key;
+    for (let key in obj) {
+        const value = obj[key];
+        const newPath = currentPath ? `${currentPath}.${key}` : key;
 
-      if (typeof value === 'object' && value !== null) {
-          findAllKeyPaths(value, targetValue, newPath, paths);
-      } else if (typeof value === 'string' && value.toLowerCase() === targetValue.toLowerCase()) {
-          paths.push(newPath);
-      }
-  }
-  return paths;
+        if (typeof value === 'object' && value !== null) {
+            findAllKeyPaths(value, targetValue, newPath, paths);
+        } else if (typeof value === 'string' && value.toLowerCase() === targetValue.toLowerCase()) {
+            paths.push(newPath);
+        }
+    }
+    return paths;
 }
 
-function replaceInFiles(directoryPattern, ignorePattern, mappings) {
-  const options = { ignore: ignorePattern };
-  const files = glob.sync(directoryPattern, options);
+function searchAndReplaceInLiquidFiles(directoryPattern, ignorePattern, mappings) {
+    const options = { ignore: ignorePattern };
+    const files = glob.sync(directoryPattern, options);
 
-  files.forEach(file => {
-      let content = fs.readFileSync(file, 'utf8');
-      let replaced = false;
+    files.forEach(file => {
+        let content = fs.readFileSync(file, 'utf8');
+        let modified = false;
 
-      for (let keyB in mappings) {
-          mappings[keyB].forEach(keyA => {
-              const regex = new RegExp(`'${keyB}'`, 'g');
-              if (regex.test(content)) {
-                  content = content.replace(regex, `'${keyA}'`);
-                  replaced = true;
-              }
-          });
-      }
+        for (let keyB in mappings) {
+            mappings[keyB].forEach(keyA => {
+                const regex = new RegExp(`'${keyB.replace(/\./g, '\\.')}'`, 'g');
+                if (regex.test(content)) {
+                    console.log(`Replacing '${keyB}' with '${keyA}' in ${file}`);
+                    content = content.replace(regex, `'${keyA}'`);
+                    modified = true;
+                }
+            });
+        }
 
-      if (replaced) {
-          fs.writeFileSync(file, content);
-      }
-  });
+        if (modified) {
+            fs.writeFileSync(file, content);
+        }
+    });
 }
 
-const [fileA, fileB, directoryPattern, ignorePattern] = process.argv.slice(2);
+const [fileAPath, fileBPath, directoryPattern, ignorePattern] = process.argv.slice(2);
+
+const fileA = readJsonFile(fileAPath);
+const fileB = readJsonFile(fileBPath);
 
 const keyMappings = {};
 for (let keyA in fileA) {
@@ -69,4 +77,5 @@ for (let keyA in fileA) {
     });
 }
 
-replaceInFiles(directoryPattern, ignorePattern, keyMappings);
+searchAndReplaceInLiquidFiles(directoryPattern, ignorePattern, keyMappings);
+
