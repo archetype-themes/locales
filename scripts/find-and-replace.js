77a409f53cf56ff3d4e75a30b31b5ae4e0bc4f1b
Thomas Kelly <thomas@archetypethemes.co>
2024-01-24 13:33:58 -0500

    Update find-and-replace script

--- scripts/find-and-replace.js
+++ scripts/find-and-replace.js
@@ -38,7 +38,9 @@ function findAllKeyPaths(obj, targetValue, currentPath = '', paths = []) {
 
 function searchAndReplaceInLiquidFiles(directoryPattern, ignorePattern, mappings) {
     const options = { ignore: ignorePattern };
+    
     const files = glob.sync(directoryPattern, options);
+
 
     files.forEach(file => {
         let content = fs.readFileSync(file, 'utf8');
@@ -46,6 +48,7 @@ function searchAndReplaceInLiquidFiles(directoryPattern, ignorePattern, mappings
 
         for (let keyB in mappings) {
             mappings[keyB].forEach(keyA => {
+                // console.log(keyB)
                 const regex = new RegExp(`'${keyB.replace(/\./g, '\\.')}'`, 'g');
                 if (regex.test(content)) {
                     console.log(`Replacing '${keyB}' with '${keyA}' in ${file}`);
@@ -63,21 +66,31 @@ function searchAndReplaceInLiquidFiles(directoryPattern, ignorePattern, mappings
 
 const [fileAPath, fileBPath, directoryPattern, ignorePattern] = process.argv.slice(2);
 
+console.log(fileAPath, fileBPath, directoryPattern, ignorePattern)
+
 const fileA = readJsonFile(fileAPath);
 const fileB = readJsonFile(fileBPath);
 
-const keyMappings = {};
-for (let keyA in fileA) {
-    const valueA = fileA[keyA];
-    const keyBs = findAllKeyPaths(fileB, valueA);
+function buildKeyMappings(obj, baseObj, baseKey = '') {
+    for (let key in obj) {
+        const value = obj[key];
+        const newBaseKey = baseKey ? `${baseKey}.${key}` : key;
 
-    keyBs.forEach(keyB => {
-        if (!keyMappings[keyB]) {
-            keyMappings[keyB] = [];
+        if (typeof value === 'object' && value !== null) {
+            buildKeyMappings(value, baseObj, newBaseKey);
+        } else {
+            const keyBs = findAllKeyPaths(baseObj, value);
+            keyBs.forEach(keyB => {
+                if (!keyMappings[keyB]) {
+                    keyMappings[keyB] = [];
+                }
+                keyMappings[keyB].push(newBaseKey);
+            });
         }
-        keyMappings[keyB].push(keyA);
-    });
+    }
 }
 
-searchAndReplaceInLiquidFiles(directoryPattern, ignorePattern, keyMappings);
+const keyMappings = {};
+buildKeyMappings(fileA, fileB);
 
+searchAndReplaceInLiquidFiles(directoryPattern, ignorePattern, keyMappings);
